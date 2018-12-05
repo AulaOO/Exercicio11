cdecf343781808c950404fb93e9ca5088a76890c
LuisGuilhermeCipriai <cipriani.luisguilherme@gmail.com>
2018-12-05 11:56:06 -0200

    Exercício 11 (Lista para prova 1)

---
+++ src/exercicio11/conversaoDeUnidadesDeArea.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio11;
+
+/**
+ *
+ * @author Luis Guilherme
+ */
+public class conversaoDeUnidadesDeArea {
+    private double valor;
+
+
+    public conversaoDeUnidadesDeArea() {
+    }
+
+    public conversaoDeUnidadesDeArea(double valor) {
+        this.valor = valor;
+
+    }
+
+    public double getMetroQuadrado() {
+        return valor;
+    }
+
+    public void setMetroQuadrado(double valor) {
+        this.valor = valor;
+    }
+    
+    
+    public static double metroQuadradoParaPesQuadrados(){
+        double d = 0;
+        return d;
+    }
+}
